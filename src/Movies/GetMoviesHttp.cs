d8dbe8a4ac11597109ea36a00c470d2865b9d708
Marc Duiker <marcduiker@gmail.com>
2019-12-09 23:42:03 +0100

    .NET Core 3 with serialization issues

--- src/Movies/GetMoviesHttp.cs
+++ src/Movies/GetMoviesHttp.cs
@@ -4,7 +4,6 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using System.Net.Http;
 using System.Linq;
 using System;
 
